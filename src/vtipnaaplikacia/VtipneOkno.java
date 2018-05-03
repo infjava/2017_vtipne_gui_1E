0b446c57177671541d222df570b94b3ea19df2d7
janik <janik@RA006-00.fri.uniza.sk>
2018-05-03 14:10:23 +0200

    Cvicenie 11B - zakladna vymena ano/nie

--- src/vtipnaaplikacia/VtipneOkno.java
+++ src/vtipnaaplikacia/VtipneOkno.java
@@ -9,6 +9,8 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -45,6 +47,18 @@ class VtipneOkno {
         nieButton.addActionListener((ActionEvent e) -> {
             JOptionPane.showMessageDialog(null, "To nemyslis vazne!");
             System.exit(0);
+        });
+        
+        nieButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                tlacidla.removeAll();
+                
+                tlacidla.add(nieButton);
+                tlacidla.add(anoButton);
+                
+                VtipneOkno.this.okno.revalidate();
+            }
         });
 
         tlacidla.add(anoButton);
