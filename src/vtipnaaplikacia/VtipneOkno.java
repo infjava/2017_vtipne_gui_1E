334268b3f4867e884aa8120c5daca1d3c34b9139
janik <janik@RA006-00.fri.uniza.sk>
2018-05-03 13:46:29 +0200

    Cvicenie 11B - layout vtipneho okna

--- src/vtipnaaplikacia/VtipneOkno.java
+++ src/vtipnaaplikacia/VtipneOkno.java
@@ -5,7 +5,12 @@
  */
 package vtipnaaplikacia;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
@@ -18,6 +23,20 @@ class VtipneOkno {
     public VtipneOkno() {
         this.okno = new JFrame("Otazka");
         this.okno.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        
+        this.okno.setLayout(new BorderLayout());
+        
+        this.okno.add(new JLabel("Spravis skusku z Informatiky?"), BorderLayout.CENTER);
+        
+        JPanel tlacidla = new JPanel();
+        tlacidla.setLayout(new GridLayout(1, 2));
+        
+        tlacidla.add(new JButton("Ano"));
+        tlacidla.add(new JButton("Nie"));
+        
+        this.okno.add(tlacidla, BorderLayout.SOUTH);
+        
+        this.okno.pack();
     }
 
     void zobraz() {
